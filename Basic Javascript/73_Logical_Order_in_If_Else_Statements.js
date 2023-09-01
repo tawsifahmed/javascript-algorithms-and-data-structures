3b622849e3180313a97bfccf42176d1a8c29adde
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2023-09-01 13:08:05 +0600

    formatted code examples on questions

--- Basic Javascript/73_Logical_Order_in_If_Else_Statements.js
+++ Basic Javascript/73_Logical_Order_in_If_Else_Statements.js
@@ -6,26 +6,26 @@ Take these two functions as an example.
 
 Here's the first:
 
-function foo(x) {
-  if (x < 1) {
-    return "Less than one";
-  } else if (x < 2) {
-    return "Less than two";
-  } else {
-    return "Greater than or equal to two";
+  function foo(x) {
+    if (x < 1) {
+      return "Less than one";
+    } else if (x < 2) {
+      return "Less than two";
+    } else {
+      return "Greater than or equal to two";
+    }
   }
-}
 And the second just switches the order of the statements:
 
-function bar(x) {
-  if (x < 2) {
-    return "Less than two";
-  } else if (x < 1) {
-    return "Less than one";
-  } else {
-    return "Greater than or equal to two";
+  function bar(x) {
+    if (x < 2) {
+      return "Less than two";
+    } else if (x < 1) {
+      return "Less than one";
+    } else {
+      return "Greater than or equal to two";
+    }
   }
-}
 While these two functions look nearly identical if we pass a number to both we get different outputs.
 
 foo(0)
